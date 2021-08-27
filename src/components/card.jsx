c4080b4c284e303611823cbeb99030e78f3520ab
Kat Beglyarova <katbeglyar@gmail.com>
2021-08-27 16:54:03 +0300

    few changes made

--- src/components/card.jsx
+++ src/components/card.jsx
@@ -1,2 +1,13 @@
 import React from "react";
+
+
+function Card(){
+    return(
+        <div className='card'>
+            <p className='card__word'>{props.preposition} {props.word}</p>
+            <p className='card__transcription'>{props.transcription}</p>
+            <p className='card__translation'>{props.translation}</p>
+        </div>
+    );
+}
 export default Card;
--- src/components/words-list.jsx
+++ src/components/words-list.jsx
@@ -1,3 +1,14 @@
 import React from "react";
 
+function WordsList(props){
+    const words = props.wordsList;
+    return(
+        <div className='wordsList'>
+            <p>Добавить новое слово +</p>
+            {words.map((w) => 
+            <p>{w.word} <sup>x</sup></p>)
+            }
+        </div>
+    );
+}
 export default WordsList;
